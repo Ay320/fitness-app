e38f32c715e2d1243e4cb301c0b5b59ecdb2059d
Ayham Ahmed <163321889+Ay320@users.noreply.github.com>
2025-04-23 18:53:41 +0100

    Add backend URL (deployed on AWS)

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 import Constants from "expo-constants";
 
-const API_BASE_URL: string = Constants.expoConfig?.extra?.API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL: string = Constants.expoConfig?.extra?.API_BASE_URL || "http://3.8.177.201:8000";
 export default API_BASE_URL;
