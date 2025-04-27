a675c9a35b3532eb4d6aced94cf0af0ae0b73d18
Ayham Ahmed <163321889+Ay320@users.noreply.github.com>
2025-04-27 15:05:46 +0100

    Redeploy backend

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "fitness-app",
     version: "1.0.0",
     extra: {
-      API_BASE_URL: "http://18.175.153.136:8000", // Default backend URL (might change later)
+      API_BASE_URL: "http://13.42.65.113:8000", // backend URL
     },
   };
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 import Constants from "expo-constants";
 
-const API_BASE_URL: string = Constants.expoConfig?.extra?.API_BASE_URL || "http://18.175.153.136:8000";
+const API_BASE_URL: string = Constants.expoConfig?.extra?.API_BASE_URL || "http://13.42.65.113:8000";
 export default API_BASE_URL;
